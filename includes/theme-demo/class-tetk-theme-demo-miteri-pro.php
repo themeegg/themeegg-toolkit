4dcd2825af4253fb19c0abcbc41a72a9d263c191
Theme Egg <themeeggofficial@gmail.com>
2018-01-24 21:01:51 +0545

    miteri pro default demoo

--- includes/theme-demo/class-tetk-theme-demo-miteri-pro.php
+++ includes/theme-demo/class-tetk-theme-demo-miteri-pro.php
@@ -11,11 +11,11 @@ class TETK_Theme_Demo_Miteri_Pro extends TETK_Theme_Demo {
 		$server_url = 'https://demo.themeegg.com/themes/miteri-pro/';
 		$demo_urls  = array(
 			array(
-				'import_file_name'           => 'Miteri Pro',
-				'import_file_url'            => $server_url . 'wp-content/themes/miteri-pro/demo-content/content.xml',
-				'import_widget_file_url'     => $server_url . 'wp-content/themes/miteri-pro/demo-content/widgets.wie',
-				'import_customizer_file_url' => $server_url . 'wp-content/themes/miteri-pro/demo-content/customizer.dat',
-				'import_preview_image_url'   => $server_url . 'wp-content/themes/miteri-pro/screenshot.png',
+				'import_file_name'           => 'Default',
+				'import_file_url'            => $server_url . 'wp-content/demo-content/default/content.xml',
+				'import_widget_file_url'     => $server_url . 'wp-content/demo-content/default/widgets.wie',
+				'import_customizer_file_url' => $server_url . 'wp-content/demo-content/default/customizer.dat',
+				'import_preview_image_url'   => $server_url . 'wp-content/demo-content/default/screenshot.png',
 				'demo_url'                   => $server_url . '',
 				//'import_notice'              => __( 'After you import this demo, you will have to setup the slider separately.', 'your-textdomain' ),
 			)
@@ -38,25 +38,27 @@ class TETK_Theme_Demo_Miteri_Pro extends TETK_Theme_Demo {
 
 		$new_theme_locations = get_registered_nav_menus();
 
+		$nav_menus = array();
+
 		foreach ( $new_theme_locations as $location_key => $location ) {
 
 			$menu = get_term_by( 'name', $location, 'nav_menu' );
 
 			if ( isset( $menu->term_id ) ) {
-				set_theme_mod( 'nav_menu_locations', array(
-						'primary' => $menu->term_id,
-					)
-				);
+
+				$nav_menus[ $location_key ] = $menu->term_id;
 			}
 		}
-
+		if ( count( $nav_menus ) > 0 ) {
+			set_theme_mod( 'nav_menu_locations', $nav_menus );
+		}
 
 // Assign front page and posts page (blog page).
-//		$front_page_id = get_page_by_title( 'Home' );
-//		$blog_page_id  = get_page_by_title( 'Blog' );
-//		update_option( 'show_on_front', 'page' );
-//		update_option( 'page_on_front', $front_page_id->ID );
-//		update_option( 'page_for_posts', $blog_page_id->ID );
+		$front_page_id = get_page_by_title( 'Home' );
+		$blog_page_id  = get_page_by_title( 'Blog' );
+		update_option( 'show_on_front', 'page' );
+		update_option( 'page_on_front', $front_page_id->ID );
+		update_option( 'page_for_posts', $blog_page_id->ID );
 		update_option( 'themeegg_themes', $installed_demos );
 	}
 }
